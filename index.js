7266b0a947d60f38d6ac8e854d7d7ab520fb4142
Steve Fallet <steve.fallet@divtec.ch>
2025-09-22 14:25:10 +0200

    ajout des CORS

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const path = require('path');
 const authRoutes = require('./routes/auth');
 const pokemonRoutes = require('./routes/pokemons');
 const typeRoutes = require('./routes/types');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3535;
 
 // Middleware pour lire le JSON dans les requêtes
 app.use(express.json());
+app.use(cors()); // autorise toutes les origines, toutes les méthodes
+app.options('*', cors());
 
 // Servir les images depuis le dossier /images
 app.use('/images', express.static(path.join(__dirname, 'images')));
